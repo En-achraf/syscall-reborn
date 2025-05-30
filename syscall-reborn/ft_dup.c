4b22d25d20181845f47f6c50d32773eaa1ac5f96
0ac_raf0dev <acennadi@student.1337.ma>
2025-05-30 16:03:48 +0100

    Dup2 reborn & update headerfile

--- syscall-reborn/ft_dup.c
+++ syscall-reborn/ft_dup.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   syscall-reborn.h                                   :+:      :+:    :+:   */
+/*   ft_dup.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acennadi <acennadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/30 15:52:30 by acennadi          #+#    #+#             */
-/*   Updated: 2025/05/30 15:56:08 by acennadi         ###   ########.fr       */
+/*   Created: 2025/05/30 15:51:23 by acennadi          #+#    #+#             */
+/*   Updated: 2025/05/30 16:00:33 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SYSCALL_REBORN_h
-#define SYSCALL_REBORN_h
+//F_DUPFD : Duplicate the file descriptor to greater than or equal to arg (newfd)
 
-//includes
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "../headers/syscall-reborn.h"
 
-//functions parametre
-int ft_dup2(int oldfd, int newfd);
-int ft_dup(int oldfd);
-
-#endif
+int ft_dup(int oldfd)
+{
+    int new_fd;
+    
+    new_fd = fcntl(oldfd, F_DUPFD, 0);
+    if (new_fd < 0)
+        return -1;
+    return (new_fd);
+}
